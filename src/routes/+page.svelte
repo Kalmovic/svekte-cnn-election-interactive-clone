a75cd2bb98eeca0f888d4eeee931ccfeec783a51
Kalmovic <joseph.ext@rallyrd.com>
2023-12-05 16:51:03 -0300

    feat: add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script>
 	import OptionCard from '../components/option-card.svelte';
-	import result2020 from '$lib/assets/2020_results.png';
+	import result2016 from '$lib/assets/2016_results.jpg';
 	import result2024 from '$lib/assets/2024_results.jpg';
 </script>
 
 <h1>Results and maps</h1>
 
 <section>
-	<a href="/results/2020">
-		<OptionCard description="Check the results of the 2020 election." img={result2020} />
+	<a href="/results/2016">
+		<OptionCard description="Check the results of the 2016 election." img={result2016} />
 	</a>
-	<a href="/road-to-270">
+	<a
+		href="https://github.com/Kalmovic/cnn-interactive-election-map"
+		rel="noopener noreferrer"
+		target="_blank"
+	>
 		<OptionCard description="Predict the next POTUS!" img={result2024} />
 	</a>
 </section>
