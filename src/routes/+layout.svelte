ce0883bcc7a965a6c21204daeb7f4ba02f4e773c
Kalmovic <joseph.ext@rallyrd.com>
2023-12-05 16:50:51 -0300

    feat: add app layout with header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
 	import '../app.css';
+	import CNN_Logo from '$lib/assets/cnn_logo.png';
 </script>
 
+<header>
+	<div class="header-container">
+		<a href="/">
+			<img src={CNN_Logo} alt="CNN Logo" width="150" />
+		</a>
+	</div>
+</header>
 <main>
 	<slot />
 </main>
@@ -10,5 +18,15 @@
 	main {
 		margin: 0 auto;
 		max-width: 960px;
+		padding: 2rem 0;
+	}
+
+	header {
+		width: 100%;
+		border: 0.5px solid #bcbcbc;
+	}
+	.header-container {
+		max-width: 960px;
+		margin: 0 auto;
 	}
 </style>
