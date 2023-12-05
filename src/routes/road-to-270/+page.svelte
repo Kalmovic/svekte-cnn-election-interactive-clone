345829f60a855b8e7732591619e6840fab27ca63
Kalmovic <joseph.ext@rallyrd.com>
2023-12-05 16:54:54 -0300

    feat: in progress road to 270 page

---
+++ src/routes/road-to-270/+page.svelte
@@ -0,0 +1,45 @@
+<header>
+	<h1>road to 270</h1>
+</header>
+<p>
+	It takes 270 electoral votes to win the 2016 presidential election. Build your own road to 270
+	starting with a map of reliably red and blue states, as well as the battlegrounds CNN is tracking
+	most closely. You also can view CNN's current race ratings or use the 2016 electoral map as a
+	starting point. Clicking an individual state will change who is winning its electoral votes.
+</p>
+
+<style>
+	header {
+		width: 100%;
+		text-align: center;
+		border: none;
+		text-transform: uppercase;
+		height: fit-content;
+	}
+	h1 {
+		font-family: Arial, Helvetica, sans-serif;
+		margin: 0;
+		width: 100%;
+		text-align: center;
+		border: 1px solid #000; /* Use your desired border color */
+		text-transform: uppercase;
+		height: fit-content;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 2rem; /* Adjust the font size as needed */
+		font-weight: bold;
+	}
+
+	p {
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-size: 1rem;
+		color: '#6E6E6E';
+	}
+</style>
