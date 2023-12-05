a5e33e7a31b7702624041728c4b4bda0462d30b7
Kalmovic <joseph.ext@rallyrd.com>
2023-12-05 16:50:30 -0300

    component us state selector

---
+++ src/components/us-state-selector.svelte
@@ -0,0 +1,24 @@
+<script>
+	let selectedState = '';
+
+	const states = [
+		'Alabama',
+		'Alaska',
+		'Arizona',
+		'Arkansas',
+		'California'
+		// ... add more states here
+	];
+
+	function handleChange(event) {
+		selectedState = event.target.value;
+	}
+</script>
+
+<select value={selectedState} on:change={(e) => handleChange(e)}>
+	{#each states as state}
+		<option value={state}>{state}</option>
+	{/each}
+</select>
+
+<p>Selected state: {selectedState}</p>
